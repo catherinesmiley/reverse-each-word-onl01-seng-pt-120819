64bf4691c43a907a0d6f40b171c6ab9f6c6964d0
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-07 20:29:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,35 @@
+require 'pry' 
+
 def reverse_each_word(array)
   new_array = array.split(" ")
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
 end
 
-def reverse_each_word(array)
-  new_array = array.split(" ").collect {|x| x.reverse!}.join(" ")
-end
+# def reverse_each_using_collect(array)
+#   array.split(" ").collect {|x| x.reverse!}.join(" ")
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reverse_each_word(string)
+#   string.reverse.split.reverse.join(" ")
+# end
+
+# def reverse_each_word(array)
+#   new_array = array.split(" ").collect {|word| word.reverse!}.join(" ")
+# end
